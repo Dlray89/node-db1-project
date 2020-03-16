5bf082ea0170ba0feac46fae7fce7a9c68d3ec84
dlray89 <dlrayjr89@gmail.com>
2020-03-16 18:46:47 -0400

    GET request works

--- api/server.js
+++ api/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
+const morgan = require('morgan')
 
 const db = require("../data/dbConfig.js");
 
-const server = express();
+const accountRouter = require("../data/router/accounts-router")
 
+const server = express();
+server.use(morgan("dev"))
 server.use(express.json());
+server.use("/api/accounts", accountRouter)
+
+server.get("/", (req,res)=> {
+    res.staus(200).json({message: "Success"})
+})
 
 module.exports = server;
--- data/router/accounts-router.js
+++ data/router/accounts-router.js
@@ -4,3 +4,15 @@ const accountDB = require("../dbConfig")
 
 const router = express.Router()
 
+router.get('/', (req,res)=> {
+    accountDB.select("*")
+    .from("accounts")
+    .then(rows => {
+        res.status(200).json({ data: rows})
+    })
+    .catch(error => {
+        res.status(500).json({errorMessage: "Could not find what your looking for"})
+    })
+})
+
+module.exports = router
